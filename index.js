bbbef884d8514435b9aa101b06334ca192df8e40
Eshad Dewan <eshaddewan571@gmail.com>
2021-10-30 20:23:41 +0600

    jj

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -19,13 +20,87 @@ async function run() {
         await client.connect();
         console.log('database connected')
         const database = client.db('travel_agency');
-        const eventCollection = database.collection('offerings');
+        const offersCollection = database.collection('offerings');
+        const bookingCollection = database.collection('bookings')
 
         // GET OFFERS API
         app.get('/offerings', async (req, res) => {
-            const cursor = eventCollection.find({});
+            const cursor = offersCollection.find({});
             const offers = await cursor.toArray();
             res.send(offers);
+        })
+
+        // POST OFFERS API
+
+        app.post('/offerings', async (req, res) => {
+
+            const newOffer = req.body;
+            const result = await offersCollection.insertOne(newOffer)
+
+            console.log('got new user', req.body);
+            console.log('added user', result);
+
+            //sending back to client side
+            res.send(result)
+        })
+
+        //GET BOOKING API
+        app.get('/bookings', async (req, res) => {
+            const cursor = bookingCollection.find({});
+            const bookings = await cursor.toArray();
+            res.send(bookings);
+        })
+
+
+        //POST BOOKING API
+        app.post('/bookings', async (req, res) => {
+
+            const newBooking = req.body;
+            const result = await bookingCollection.insertOne(newBooking)
+
+            res.send(result)
+        })
+
+        // GET MY BOOKING API
+        app.get('/bookings/:email', async (req, res) => {
+            const query = req.params.email;
+
+            const cursor = bookingCollection.find({ email: query });
+            const bookings = await cursor.toArray();
+            res.send(bookings);
+        })
+
+        //UPDATE API
+
+        app.put('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedBooking = req.body;
+            console.log(id, updatedBooking)
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    status: updatedBooking.status,
+
+
+                }
+            };
+            const result = await bookingCollection.updateOne(filter, updateDoc, options)
+
+            res.send(result);
+        })
+
+        //DELETE API
+
+        app.delete('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = { _id: ObjectId(id) };
+            const result = await bookingCollection.deleteOne(query);
+            // console.log('deleted count', result)
+            // console.log('deleting user with id', id)
+
+            res.json(result);
         })
 
     }
